4568ba7fc8216e929ce0352ad829bd0d91999242
Bayu Permana Putra <bayuputra9a@gmail.com>
2019-12-13 16:47:36 +0700

    first commit

---
+++ app/src/main/java/com/example/moviecatalogue/MovieInterface.kt
@@ -0,0 +1,10 @@
+package com.example.moviecatalogue
+
+data class MovieInterface(
+    var title: String = "",
+    var release: String = "",
+    var description: String = "",
+    var score: String = "",
+    var caster: String = "",
+    var director: String = ""
+)
